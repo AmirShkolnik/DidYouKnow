e4fb3eb37db85b679ad8d57f00f1ec5035ea76dc
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-01-23 17:39:05 +0000

    js - remove buttons code, add hide welcome, show rules. indes - add back btn and rules text, remove back btn from welcome lp

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,25 @@
    // Function to change the background image of the div
    function rules() {
-    var boxRules = document.getElementsByClassName('quiz_area')[0];
-        boxRules.style.backgroundImage = "url('assets/images/test.jpg')";
 
-        let headlineHome = document.getElementById('headline_home');
-        headlineHome.innerHTML = "How To Survive the Jungle Quiz Safari!";
-
-        let playBtn = document.getElementById('play_btn');
-        if (playBtn) {
-            playBtn.style.display = "none";
-        }
-
-        let rulesBtn = document.getElementById('rules_btn');
-        if (rulesBtn) {
-            rulesBtn.style.display = "none";
-        }
+    // Hide welcome section
+    let welcomeSection = document.getElementById('quiz_area_box');
+    if (welcomeSection) {
+        welcomeSection.style.display = "none";
     }
 
-function goBack() {
-  // Implement logic to revert changes or navigate back
-  // For example, you can reload the page or use history.back()
-  // You can customize this based on your specific needs
-  location.reload(); // Example: Reload the page
+    // Show rules section
+    let rulesHide = document.getElementById('rules_hide');
+    if (rulesHide) {
+      rulesHide.style.display = "block";
+      rulesHide.style.backgroundImage = "url('assets/images/test.jpg')";
+    }
 }
+
+function goBack() {
+ 
+     // Implement logic to revert changes or navigate back
+     // For example, you can reload the page or use history.back()
+     // You can customize this based on your specific needs
+     location.reload(); // Example: Reload the page
+   }
+  
