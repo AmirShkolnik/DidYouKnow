f16a954829bc1309f81c40fe2c48a6e80d92ca0c
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-01-25 08:53:33 +0000

    index- remove buttons ids to same button class. style - styling buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,22 @@
-
-// Back Button - Reload the page
 function goBack() {
- 
-     location.reload();
-   }
+    'use strict';
+    window.location.reload();
+}
 
-   function rulesnew() {
-   let rulesNew = document.getElementById('quiz_area_box');
-   rulesNew.innerHTML = `
-        <div><h1 id="headline_home" class="heading">How To Survive the Jungle Quiz Safari!</h1></div>
-        <div class="quiz-button">
-   <p>A fun and fact-filled questionnaire that will challenge your knowledge about the chemistry we are surrounded with.</p>
-       <p>This quiz consists of Multiple Choice Questions. Use your best guess and click on the answer.</p>
-           <p>Right answers will appear green and wrong ones in red. Scores will appear right away.</p>
-               <p>Ready to challenge yourself?</p>
-               <button id='back_btn' aria-label="back" onclick="goBack()">Back</button>
-</div>
-`;
-   }
+function rulesnew() {
+    'use strict';
+    let rulesNew = document.getElementById('quiz_area_box');
+    if (rulesNew) {
+        rulesNew.innerHTML = `
+            <div><h1 id="headline_home" class="heading">How To Survive the Jungle Quiz Safari!</h1></div>
+            <div class="quiz-button">
+                <p>A fun and fact-filled questionnaire that will challenge your knowledge about the chemistry we are surrounded with.</p>
+                <p>This quiz consists of Multiple Choice Questions. Use your best guess and click on the answer.</p>
+                <p>Right answers will appear green and wrong ones in red. Scores will appear right away.</p>
+                <p>Ready to challenge yourself?</p>
+                <button  id="back_btn" class="button" aria-label="back" onclick="goBack()">Back</button>
+            </div>
+        `;
+    }
+}
+
