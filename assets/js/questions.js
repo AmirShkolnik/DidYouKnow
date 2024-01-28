0dd2c8205757e3e5c6e9cb62e1dada5f50487ace
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-01-28 11:43:08 +0000

    moving all questions to new file questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,103 @@
+// Jungle quiz questions
+//Question numer 1
+const questions = [{
+    question: "What is the largest animal in the jungle?",
+    answers: [{
+            text: "Elephant",
+            correct: true
+        },
+        {
+            text: "Gorilla",
+            correct: false
+        },
+        {
+            text: "Hippopotamus",
+            correct: false
+        },
+        {
+            text: "Tiger",
+            correct: false
+        },
+    ]
+},
+{ // Question numer 2
+    question: "What is the most venomous snake found in the jungle?",
+    answers: [{
+            text: "Cobra",
+            correct: false
+        },
+        {
+            text: "Viper",
+            correct: false
+        },
+        {
+            text: "Mamba",
+            correct: false
+        },
+        {
+            text: "Taipan",
+            correct: true
+        },
+    ]
+},
+{ // Question numer 3
+    question: "Which bird is known for imitating sounds and voices?",
+    answers: [{
+            text: "Hawk",
+            correct: false
+        },
+        {
+            text: "Parrot",
+            correct: true
+        },
+        {
+            text: "Toucan",
+            correct: false
+        },
+        {
+            text: "Owl",
+            correct: false
+        },
+    ]
+},
+{ // Question numer 4
+    question: "What is the primary source of food for many jungle animals?",
+    answers: [{
+            text: "Grass",
+            correct: false
+        },
+        {
+            text: "Fruits",
+            correct: true
+        },
+        {
+            text: "Insects",
+            correct: false
+        },
+        {
+            text: "Fish",
+            correct: false
+        },
+    ]
+},
+{ // Question numer 5
+    question: "Which is the most dangerous animal in the jungle?",
+    answers: [{
+            text: "Lion",
+            correct: false
+        },
+        {
+            text: "Anaconda",
+            correct: false
+        },
+        {
+            text: "Crocodile",
+            correct: true
+        },
+        {
+            text: "Rhino",
+            correct: false
+        },
+    ]
+}
+];
