145045b862804518b2dbcf6cc398c1281a15781d
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-02-02 10:01:44 +0000

    questions.js removed and united with script.js for better practice and less validerings warnings

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,407 @@
+// Jungle quiz questions
+//Question numer 1
+let questions = [{
+    question: "Which animal holds the title for being the largest in the jungle?",
+    answers: [{
+            text: "Elephant",
+            correct: true
+        },
+        {
+            text: "Gorilla",
+            correct: false
+        },
+        {
+            text: "Hippopotamus",
+            correct: false
+        },
+        {
+            text: "Tiger",
+            correct: false
+        },
+    ]
+},
+{ // Question numer 2
+    question: "What is the deadliest snake found in the jungle?",
+    answers: [{
+            text: "Cobra",
+            correct: false
+        },
+        {
+            text: "Viper",
+            correct: false
+        },
+        {
+            text: "Mamba",
+            correct: false
+        },
+        {
+            text: "Taipan",
+            correct: true
+        },
+    ]
+},
+{ // Question numer 3
+    question: "Which bird is renowned for mimicking sounds and voices?",
+    answers: [{
+            text: "Hawk",
+            correct: false
+        },
+        {
+            text: "Parrot",
+            correct: true
+        },
+        {
+            text: "Toucan",
+            correct: false
+        },
+        {
+            text: "Owl",
+            correct: false
+        },
+    ]
+},
+{ // Question numer 4
+    question: "What is the primary food source for many animals in the jungle?",
+    answers: [{
+            text: "Grass",
+            correct: false
+        },
+        {
+            text: "Fruits",
+            correct: true
+        },
+        {
+            text: "Insects",
+            correct: false
+        },
+        {
+            text: "Fish",
+            correct: false
+        },
+    ]
+},
+{ // Question numer 5
+    question: "What is the most hazardous animal in the jungle?",
+    answers: [{
+            text: "Lion",
+            correct: false
+        },
+        {
+            text: "Anaconda",
+            correct: false
+        },
+        {
+            text: "Crocodile",
+            correct: true
+        },
+        {
+            text: "Rhino",
+            correct: false
+        },
+    ]
+},
+{ // Question numer 6
+    question: "What percentage of the Earth's surface is covered with jungle and rainforest?",
+    answers: [{
+            text: "2%",
+            correct: false
+        },
+        {
+            text: "15%",
+            correct: false
+        },
+        {
+            text: "30%",
+            correct: false
+        },
+        {
+            text: "6%",
+            correct: true
+        },
+    ]
+},
+{ // Question numer 7
+    question: "The floor of the jungle is covered in___.",
+    answers: [{
+            text: "Leaves",
+            correct: false
+        },
+        {
+            text: "Tall grass",
+            correct: false
+        },
+        {
+            text: "Insects",
+            correct: true
+        },
+        {
+            text: "Stones",
+            correct: false
+        },
+    ]
+},
+{ // Question numer 8
+    question: 'The term "jungle" originates from the Sanskrit word "jangala". What does it mean?',
+    answers: [{
+            text: "Ocean",
+            correct: false
+        },
+        {
+            text: "Wet land",
+            correct: false
+        },
+        {
+            text: "Dry land",
+            correct: true
+        },
+        {
+            text: "Diverse land",
+            correct: false
+        },
+    ]
+},
+{ // Question numer 9
+    question: "How many countries and areas does the Amazon rainforest cover?",
+    answers: [{
+            text: "9",
+            correct: true
+        },
+        {
+            text: "5",
+            correct: false
+        },
+        {
+            text: "15",
+            correct: false
+        },
+        {
+            text: "35",
+            correct: false
+        },
+    ]
+},
+{ // Question numer 10
+    question: "In which part of the world do most tigers live in the jungle?",
+    answers: [{
+            text: "The North Pole",
+            correct: false
+        },
+        {
+            text: "North America",
+            correct: false
+        },
+        {
+            text: "Africa",
+            correct: false
+        },
+        {
+            text: "Asia",
+            correct: true
+        },
+    ]
+},
+{ // Question numer 11
+    question: "In the jungle, there is a well-known fish called the ___.",
+    answers: [{
+            text: "Salmon",
+            correct: false
+        },
+        {
+            text: "Shark",
+            correct: false
+        },
+        {
+            text: "Piranha",
+            correct: true
+        },
+        {
+            text: "Squid",
+            correct: false
+        },
+    ]
+},
+{ // Question numer 12
+    question: "In which part of the Earth are jungles mostly found?",
+    answers: [{
+            text: "Close to the oceans",
+            correct: false
+        },
+        {
+            text: "Close to the equator",
+            correct: true
+        },
+        {
+            text: "Close to the Artic circles",
+            correct: false
+        },
+        {
+            text: "Close To The Sahara Desert ",
+            correct: false
+        },
+    ]
+},
+{ // Question numer 13
+    question: "Which animal is the most dangerous in the African jungle?",
+    answers: [{
+            text: "Leopard",
+            correct: true
+        },
+        {
+            text: "Gorilla",
+            correct: false
+        },
+        {
+            text: "Elephant",
+            correct: false
+        },
+        {
+            text: "African black sugar ant",
+            correct: false
+        },
+    ]
+},
+{ // Question numer 14
+    question: "What distinguishes a rainforest from a jungle?",
+    answers: [{
+            text: "Jungles have fewer trees and more sunlight",
+            correct: false
+        },
+        {
+            text: "Rainforests are exclusive to Africa",
+            correct: false
+        },
+        {
+            text: "Rainforests get more rain",
+            correct: true
+        },
+        {
+            text: "Jungles are bigger",
+            correct: false
+        },
+    ]
+},
+{ // Question numer 15
+    question: "Which dangerous fish lives in the African jungle?",
+    answers: [{
+            text: "Killer whale",
+            correct: false
+        },
+        {
+            text: "Tiger fish",
+            correct: true
+        },
+        {
+            text: "Crocodile",
+            correct: false
+        },
+        {
+            text: "Hammerhead shark",
+            correct: false
+        },
+    ]
+},
+{ // Question numer 16
+    question: "What type of bird is a cockatoo?",
+    answers: [{
+            text: "Insect",
+            correct: false
+        },
+        {
+            text: "Owl",
+            correct: false
+        },
+        {
+            text: "Eagle",
+            correct: false
+        },
+        {
+            text: "Parrot",
+            correct: true
+        },
+    ]
+},
+{ // Question numer 17
+    question: "What does the orangutan like the most?",
+    answers: [{
+            text: "New Guinea rainforest",
+            correct: false
+        },
+        {
+            text: "The rainforest",
+            correct: false
+        },
+        {
+            text: "The African jungle",
+            correct: false
+        },
+        {
+            text: "The Asian jungle",
+            correct: true
+        },
+    ]
+},
+{ // Question numer 18
+    question: "In which part of the world do tigers make their home?",
+    answers: [{
+            text: "Africa",
+            correct: false
+        },
+        {
+            text: "South America",
+            correct: false
+        },
+        {
+            text: "Asia",
+            correct: true
+        },
+        {
+            text: "Southern Europe",
+            correct: false
+        },
+    ]
+},
+{ // Question numer 19
+    question: "Who is the author of 'The Jungle Book'?",
+    answers: [{
+            text: "JK Rowling",
+            correct: false
+        },
+        {
+            text: "CS Lewis",
+            correct: false
+        },
+        {
+            text: "Jacqueline Wilson",
+            correct: false
+        },
+        {
+            text: "Rudyard Kipling",
+            correct: true
+        },
+    ]
+},
+{ // Question numer 20
+    question: "How many animals in the world live in the jungle?",
+    answers: [{
+            text: "Over 20%",
+            correct: false
+        },
+        {
+            text: "Over 50%",
+            correct: true
+        },
+        {
+            text: "Over 80%",
+            correct: false
+        },
+        {
+            text: "Over 5%",
+            correct: false
+        },
+    ]
+}
+];
+
 // Question
 const questionElement = document.getElementById("question");
 
